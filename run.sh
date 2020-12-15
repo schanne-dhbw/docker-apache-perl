654f3fb68a8f627fc7d6600d2e6ce091097fc761
Marc Schanne <marc@schanne.org>
2020-12-15 06:13:38 +0100

    only sources

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -v $PWD/www:/var/www/html -p 8080:80 -d docker-apache-perl /usr/sbin/apache2ctl -D FOREGROUND
+docker run -v $PWD/www:/var/www/html -p 8888:80 -d docker-apache-perl /usr/sbin/apache2ctl -D FOREGROUND
